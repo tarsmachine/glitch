f8ebe8fa843c15a17fad67f53f9036c43e5a14e0
Aidan Skye <aidanskye42@gmail.com>
2019-05-09 23:24:54 -0700

    Text when not following anyone

--- frontend/components/user/follows.jsx
+++ frontend/components/user/follows.jsx
@@ -9,8 +9,15 @@ class Follows extends React.Component{
     this.props.fetchFollows();
   }
   render(){
+    if(this.props.follows.length == 0) return (
+      <div className="directory">
+        <h1>Following</h1>
+        <h2>You are not following anyone!</h2>
+      </div>
+    )
     return (
       <div className="directory">
+        <h1>Following</h1>
         <ul>
           {this.props.follows.map(follow=><li key={follow.id}><UserIndexItem user={follow} /></li>)}
         </ul>
